7f7555bc2982777d7b33f93800fc127a046bf322
cflorido <c.florido@uniandes.edu.co>
2025-03-11 19:34:24 -0500

    terminado

--- src/components/job.js
+++ src/components/job.js
@@ -2,23 +2,21 @@ import React from "react";
 import { FormattedDate, FormattedNumber, FormattedMessage } from "react-intl";
 
 const Job = ({ offer, locale }) => {
-
-  const millionText = locale === "es" ? (offer.salary === 1 ? "millón" : "millones") : "million";
-
   return (
     <tr>
       <th scope="row">{offer.id}</th>
       <td>
-        <FormattedMessage id={`Position.${offer.name}`} defaultMessage={offer.name} />
+  <FormattedMessage id={`Position.${offer.name}`} defaultMessage={offer.name} />
+</td>
+      <td>{offer.company}</td>
+      <td>
+        <FormattedNumber value={offer.salary} />{" "}
+        <FormattedMessage
+          id={locale === "es" ? (offer.salary === 1 ? "Million" : "Milliones") : "Million"}
+        />
       </td>
       <td>
-        <FormattedMessage id={`Company.${offer.company}`} defaultMessage={offer.company} />
-      </td>
-      <td>
-        <FormattedNumber value={offer.salary} /> {millionText}
-      </td>
-      <td>
-        <FormattedMessage id={`City.${offer.city}`} defaultMessage={offer.city} />
+        <FormattedMessage id={offer.city} defaultMessage={offer.city} />
       </td>
       <td>
         <FormattedDate
@@ -30,8 +28,9 @@ const Job = ({ offer, locale }) => {
         />
       </td>
       <td>
-        <FormattedNumber value={offer.views} />
-      </td>
+      <FormattedNumber value={offer.views} useGrouping={true} />
+    </td>
+
     </tr>
   );
 };
